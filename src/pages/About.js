e021874c9b2e7eaf2e37e4896a52de831ff8178e
Sherlok <sergei_malin@yahoo.com>
2022-03-02 10:40:43 -0600

    cocktails project

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+        cupiditate accusamus aspernatur, ex illum minima impedit. Porro tempore
+        impedit blanditiis deserunt sint beatae dolores voluptatibus aspernatur
+        voluptate eaque saepe, cum necessitatibus laboriosam nostrum molestiae!
+        Quas, repellendus sequi in ipsum vitae id a fugiat, numquam odio officia
+        libero facere, optio corporis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
